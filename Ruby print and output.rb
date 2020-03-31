58772b01ed4436812f1664e77e0b9615651d99f7
Sunday Abam jnr <60232994+SundayAbamjnr@users.noreply.github.com>
2020-03-31 11:52:08 +0100

    Add files via upload

---
+++ Ruby print and output.rb
@@ -0,0 +1,12 @@
+print "What's your first name? "
+first_name = gets.chomp
+puts"what ia your#{first_name}!"
+print "What's your last name? "
+last_name = gets.chomp
+puts"what is your#{last_name}!"
+print "What city are you from? "
+city = gets.chomp
+puts"what #{city }are you from?"
+print "What state or province are you from? "
+state = gets.chomp
+puts "what #{state or province} are you from?"
